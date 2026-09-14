05e9623c46ce98e51fbe3a8ceb327d5200af669c
Joaquín Ferrada Castillo <133885139+joaferrada@users.noreply.github.com>
2026-09-14 06:58:41 -0300

    cambios login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,21 @@
 import { Routes } from '@angular/router';
-import { ReservationsComponent } from './reservations.component';
-import { MsalGuard } from '@azure/msal-angular';
+
 import { HomeComponent } from './home.component';
+import { ReservationsComponent } from './reservations.component';
+
+import { MsalGuard } from '@azure/msal-angular';
 
 export const routes: Routes = [
+
+  {
+    path: '',
+    component: HomeComponent
+  },
 
   {
     path: 'reservations',
     component: ReservationsComponent,
     canActivate: [MsalGuard]
-  },
-
-  {
-    path: '',
-    pathMatch: 'full',
-    component: HomeComponent
   },
 
   {
