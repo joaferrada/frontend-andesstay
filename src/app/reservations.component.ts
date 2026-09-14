93ace5da3390989e4a6225f9b99f912a42e856f5
Joaquín Ferrada Castillo <133885139+joaferrada@users.noreply.github.com>
2026-09-14 19:32:38 -0300

    ahora si

--- src/app/reservations.component.ts
+++ src/app/reservations.component.ts
@@ -20,7 +20,7 @@ import { ReservationService } from '../services/reservation.service';
         border-radius: 8px;
         background: #f8f9fa;
       ">
-        <h3>Registrar Nueva Reservass</h3>
+        <h3>Registrar Nueva Reservas/h3>
 
         <div style="
           display: flex;
@@ -79,7 +79,7 @@ import { ReservationService } from '../services/reservation.service';
               cursor: pointer;
             "
           >
-            Guardars reserva
+            Guardarsss reserva
           </button>
 
         </div>
